67964317212af68f4be7b1d1bf74f1387ab71990
Abby <155987828+aeadn@users.noreply.github.com>
2025-02-14 14:40:13 +0100

    barre de vie

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image fillImage;
+    public PlayerHealth healthPlayer;
+    public Gradient lifeColorGradient;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lifeRatio = ((float) healthPlayer.currentLifePoints / (float) healthPlayer.maxLifePoints);
+        fillImage.fillAmount = lifeRatio;
+        fillImage.color = lifeColorGradient.Evaluate(lifeRatio);
+        
+    }
+}
